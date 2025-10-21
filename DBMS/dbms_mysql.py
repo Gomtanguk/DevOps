9b086b67346d68a288ad7eb01aaf95eaab24df94
Gomtanguk <wndyd293@gmail.com>
2025-10-21 14:31:30 +0900

    new file:   DBMS/dbms_mysql.py

---
+++ DBMS/dbms_mysql.py
@@ -0,0 +1,20 @@
+# dbms_mysql.py
+
+import pymysql
+
+conn = pymysql.connect(
+    host = "localhost",
+    user = "root",
+    password="1234",
+    database="exampledb",
+    charset="utf8mb4",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT DATABASE()")
+
+print("현재 데이터베이스: ", cursor.fetchone())
+
+conn.close()
